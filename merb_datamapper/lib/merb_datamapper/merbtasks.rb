707293422586079230ed5c3e5124c43f3fbfb651
Lance Carlson <lancecarlson@gmail.com>
2009-01-24 22:04:43 -0800

    Replaced deprecated methods

--- merb_datamapper/lib/merb_datamapper/merbtasks.rb
+++ merb_datamapper/lib/merb_datamapper/merbtasks.rb
@@ -16,11 +16,11 @@ namespace :db do
   end
   desc "Perform automigration"
   task :automigrate => :merb_env do
-    ::DataMapper::AutoMigrator.auto_migrate
+    ::DataMapper::AutoMigrator.auto_migrate!
   end
   desc "Perform non destructive automigration"
   task :autoupgrade => :merb_env do
-    ::DataMapper::AutoMigrator.auto_upgrade
+    ::DataMapper::AutoMigrator.auto_upgrade!
   end
 
   namespace :migrate do
